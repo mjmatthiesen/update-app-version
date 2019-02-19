96f4be11ae124d1d5a737c48bd2454c984b1d4cc
Matt <mmatthiesen@dmsiworks.com>
2019-02-19 14:58:36 -0700

    Initial version

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,31 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "updateappversion" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+	
+	// workspace must be defined
+	if (vscode.workspace.workspaceFolders) {
+		// app.json should live in root
+		const manifestUri = vscode.workspace.workspaceFolders[0].uri.fsPath + '\\app.json';
+		vscode.window.showInformationMessage(manifestUri);
+		// get manifest json
+		let manifest = JSON.parse(fs.readFileSync(manifestUri).toString());
+		// get most minor version
+		let split = manifest['version'].split('.');
+		// only if it exists
+		if (split[3]) {
+			// get current date time localized to mst in YYMMDDHHMM
+			split[3] = new Date().toLocaleString('en-CA', { timeZone: 'America/Edmonton', hour12: false, year: '2-digit', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit'}).replace(/\D+/g, '');
+			// update version and save
+			manifest['version'] = split.join('.');
+			// 2 = beautify of json file
+			fs.writeFileSync(manifestUri, JSON.stringify(manifest, null, 2));
+		}
+	}
 }
 
 // this method is called when your extension is deactivated
